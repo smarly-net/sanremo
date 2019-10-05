a5fc64852cd40f27834614d838933cf0cfeffc44
Denis Dmitriev <d.dmitriev@godeltech.com>
2019-10-05 18:59:40 +0300

    api ping pong

---
+++ src/Sanremo.Web/Controllers/PingController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sanremo.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PingController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "Pong";
+        }
+
+        [HttpPost]
+        public ActionResult<string> Post([FromBody] string value)
+        {
+            return $"Pong for 'value' {value}";
+        }
+    }
+}
